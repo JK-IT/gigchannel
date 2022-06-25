fe43e68c3e2259a45e51fb5cddf08e33044aeb51
dkit <local@dkit.com>
2022-06-25 11:34:55 -0700

    gig channel

---
+++ app/src/main/java/kodojo/gigchannel/control/EntryFragment.kt
@@ -0,0 +1,28 @@
+package kodojo.gigchannel.control
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kodojo.gigchannel.R
+
+
+class EntryFragment : Fragment()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_entry, container, false)
+    }
+
+}
